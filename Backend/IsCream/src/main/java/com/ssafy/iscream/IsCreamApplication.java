8f52fd079d940ba02e06284e08c716632f4e2120
권민채 <61741340+MinchaeKwon@users.noreply.github.com>
2025-01-22 14:30:52 +0900

    init spring project

---
+++ Backend/IsCream/src/main/java/com/ssafy/iscream/IsCreamApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.iscream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsCreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsCreamApplication.class, args);
+    }
+
+}
---
+++ Backend/IsCream/src/test/java/com/ssafy/iscream/IsCreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.iscream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsCreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
