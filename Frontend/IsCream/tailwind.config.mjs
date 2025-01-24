20076570ea61f75b5c43c97f606f03e8a1e269c9
박보성 <qkrqhd23@naver.com>
2025-01-24 16:04:57 +0900

    Docs:FE컨벤션 작성 및 프로젝트 생성

---
+++ Frontend/IsCream/tailwind.config.mjs
@@ -0,0 +1,12 @@
+// tailwind.config.mjs
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        custom: ["MyFont", "sans-serif"]
+      }
+    }
+  },
+  plugins: []
+};
