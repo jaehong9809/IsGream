ce6fb73ea092da4b0fb339a4c4e5a25cbacc53b6
yujeong <hao_yj@naver.com>
2025-01-31 17:38:35 +0900

    충돌방지용 커밋

--- Frontend/IsCream/src/App.tsx
+++ Frontend/IsCream/src/App.tsx
@@ -1,7 +1,7 @@
 // import Calendar from "./components/calendar/Calendar";
 import Nav from "./components/nav/Nav";
 function App() {
-  const values = [15, 8, 25];
+ 
   return (
     <>
       {/* <Calendar /> */}
