bd38cf21b2f8de45e2c5e228664bea305d8713ae
eyeben <62013201+eyeben@users.noreply.github.com>
2025-02-01 22:07:06 +0900

    feat - Controller Res 추가
    Res 추가, 브렌치명 변경

--- Backend/IsCream/src/main/java/com/ssafy/iscream/calendar/dto/response/CalendarGetDetailRes.java
+++ Backend/IsCream/src/main/java/com/ssafy/iscream/calendar/dto/response/CalendarGetDetailRes.java
@@ -1,4 +1,13 @@
 package com.ssafy.iscream.calendar.dto.response;
 
+@Data
 public class CalendarGetDetailRes {
+    boolean isMemo;
+    boolean isHtp;
+    String houseUrl;
+    String treeUrl;
+    String personUrl;
+    String report;
+    int memoId;
+    String memo;
 }
--- Backend/IsCream/src/main/java/com/ssafy/iscream/calendar/dto/response/CalendarGetRes.java
+++ Backend/IsCream/src/main/java/com/ssafy/iscream/calendar/dto/response/CalendarGetRes.java
@@ -1,4 +1,8 @@
 package com.ssafy.iscream.calendar.dto.response;
 
 public class CalendarGetRes {
+
+    enum emoji;
+    boolean isMemo;
+    boolean isHtp;
 }
