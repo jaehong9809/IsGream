f896fc4343bdafc7ac57b89b42224cbd8c502583
권민채 <61741340+MinchaeKwon@users.noreply.github.com>
2025-02-03 12:31:33 +0900

    Fix: User Controller -> RestController 수정

--- Backend/IsCream/src/main/java/com/ssafy/iscream/user/controller/UserController.java
+++ Backend/IsCream/src/main/java/com/ssafy/iscream/user/controller/UserController.java
@@ -14,13 +14,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
