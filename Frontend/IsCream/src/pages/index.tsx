649673d9cad8f94c4c194bb78b1966563bab6c45
seo <jjkkgh123@gmail.com>
2025-02-03 17:58:37 +0900

    로그인 페이지 구현

--- Frontend/IsCream/src/pages/index.tsx
+++ Frontend/IsCream/src/pages/index.tsx
@@ -1,4 +1,5 @@
 export { default as CalendarPage } from './CalendarPage';
 export { default as MyReport } from './mypage/MyReport';
 export { default as ChangeInfo } from './mypage/ChangeInfo';
+export { default as LoginPage } from './LoginPage';
 // export { default as MyPage } from "./MyPage";
