fdba2f076d38b85624d3a318457b660cd78caf24
LeeJaeHong <0915jhlee@daum.net>
2025-02-03 23:48:19 +0900

    Feat: 모델 원격 레포지토리 선언

--- AI/app/model/yolo_models.py
+++ AI/app/model/yolo_models.py
@@ -12,7 +12,7 @@ def load_model(model_name):
     model_path = LOCAL_MODEL_DIR / f"{model_name}.pt"
     if not model_path.exists():
         raise FileNotFoundError(f"Model file not found at {model_path}")
-    return torch.hub.load('ultralytics/yolov5', 'custom', path=str(model_path), force_reload=True)
+    return torch.hub.load('ultralytics/yolov5', 'custom', path=str(model_path), force_reload=True, trust_repo=True)
 
 # 모델 로드
 house_model = load_model("house")
