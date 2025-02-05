b3de8b294f4f5851c1a30bc2daa46833bb985dce
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2025-02-05 09:41:13 +0900

    Feat: Swagger 배포 포트번호 추가

--- Backend/IsCream/src/main/java/com/ssafy/iscream/common/config/SwaggerConfig.java
+++ Backend/IsCream/src/main/java/com/ssafy/iscream/common/config/SwaggerConfig.java
@@ -20,10 +20,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@OpenAPIDefinition(servers = {
-////        @Server(url = "https://{deployServerUrl}", description = "deploy server"),
-//        @Server(url = "http://localhost:8080", description = "local server")
-//})
+@OpenAPIDefinition(servers = {
+        @Server(url = "https://3.36.67.41:8081", description = "deploy server"),
+        @Server(url = "http://localhost:8080", description = "local server")
+})
 public class SwaggerConfig {
 
     @Bean
