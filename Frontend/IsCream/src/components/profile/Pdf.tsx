8bed88d72dbc592db4f112955a6278829dfac15e
yujeong <hao_yj@naver.com>
2025-02-05 12:40:44 +0900

    Fix: input 컴포넌트 변경

--- Frontend/IsCream/src/components/profile/Pdf.tsx
+++ Frontend/IsCream/src/components/profile/Pdf.tsx
@@ -1,53 +1,30 @@
+import { useNavigate } from 'react-router-dom';
 
+const Pdf = () => {
+    const navigate = useNavigate();
 
-const Pdf = () => (
-    <div className='w-full flex justify-center'>
-        <div className='w-full max-w-[706px] p-3 my-3 bg-white border border-gray-300 rounded items-center'>
-            <div className="flex justify-between">
-                <div className="m-3 text-xl">
-                    검사결과(PDF) 다운
-                </div>
-                <div className="m-3 text-xl">
-                        <button                         >
-                            &gt;
-                        </button>
+    const handleClick = () => {
+        console.log('PDF 다운로드 페이지 이동');
+        navigate('/mypage/PDFDownload');
+        
+    };
+    return (
+        <div className='w-full flex justify-center'>
+            <div className='w-full max-w-[706px] p-3 my-3 bg-white border border-gray-300 rounded items-center'>
+                <div className="flex justify-between">
+                    <div className="m-3 text-xl">
+                        검사결과(PDF) 다운
+                    </div>
+                    <div className="m-3 text-xl">
+                            <button 
+                            onClick={handleClick}>
+                                &gt;
+                            </button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-);
+    )
+};
 
 export default Pdf;
-
-{/* <div className="w-full flex justify-center">
-        <div className="w-full max-w-[706px] p-3 my-3 bg-white border border-gray-300 rounded items-center">
-            <div className="flex justify-between">
-                <div className="m-3 text-xl">
-                    자녀 정보
-                </div>
-                <div className="m-3 text-xl">
-                    {children.length < 2 && (
-                        <button 
-                            onClick={onAddChild}
-                        >
-                            +
-                        </button>
-                    )}
-                </div>
-            </div>
-            <div className="flex">
-                {children.map((child, index) => (
-                    <div className="m-3 key={index}">
-                        <ChildInfo
-                            key={index}
-                            childNickName={child.childNickname}
-                            childSex={child.childSex}
-                            childBirth={child.childBirth}
-                            onEdit={() => onEditChild(index)}
-                            onDelete={() => onDeleteChild(index)}
-                        />
-                    </div>
-                ))}
-            </div>
-        </div>
-    </div> */}
