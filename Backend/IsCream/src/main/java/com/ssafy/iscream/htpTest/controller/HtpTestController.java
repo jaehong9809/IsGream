7765689804019594ca6da9e28026ffab06fdac53
jaehong9809 <83408593+jaehong9809@users.noreply.github.com>
2025-02-05 14:38:35 +0900

    Feat: HtpTest Controller 및 Dto 일부 작성

--- Backend/IsCream/src/main/java/com/ssafy/iscream/htpTest/controller/HtpTestController.java
+++ Backend/IsCream/src/main/java/com/ssafy/iscream/htpTest/controller/HtpTestController.java
@@ -1,5 +1,24 @@
 package com.ssafy.iscream.htpTest.controller;
 
+import com.ssafy.iscream.auth.user.Login;
+import com.ssafy.iscream.common.util.ResponseUtil;
+import com.ssafy.iscream.htpTest.domain.request.HtpTestCreateReq;
+import com.ssafy.iscream.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/htp-tests")
+@RestController
+@RequiredArgsConstructor
 public class HtpTestController {
+
+    @PostMapping("/img")
+    public ResponseEntity<?> img(@Login User user, HtpTestCreateReq createReq) {
+
+        return ResponseUtil.success();
+    }
+
 }
