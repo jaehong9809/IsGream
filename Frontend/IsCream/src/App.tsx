cb285a9615e8e20e09b754cb8ba4bda7d90597ff
yujeong <hao_yj@naver.com>
2025-02-09 17:54:09 +0900

    충돌해결위한 브랜치 변경

--- Frontend/IsCream/src/App.tsx
+++ Frontend/IsCream/src/App.tsx
@@ -1,3 +1,5 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@/utils/common/queryClient';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BottomNavigation from "./components/nav/Nav";
 import Header from "./components/header/Header";
--- Frontend/IsCream/src/pages/ChatPage.tsx
+++ Frontend/IsCream/src/pages/ChatPage.tsx
@@ -1,10 +1,41 @@
+import { useNavigate } from "react-router-dom";
 
+interface ChatPageProps{
+    chatId: string;
+    chatName?: string;
+}
 const ChatPage = () => {
+
+    const navigate = useNavigate();
 
     return(
         <div>
             채팅페이지
         </div>
+        // <div className="flex flex-col h-screen bg- 32hite">
+        //     <header className="p-4 border-b">
+        //         <h1 className="text-xl">채팅</h1>
+        //     </header>
+            
+        //     <div className="flex-1 overflow-y-auto">
+        //         {chatList.map((chat) => (
+        //             <div
+        //                 key={chat.id}
+        //                 onClick={() => navigate(`/chat/${chat.id}`)}
+        //                 className="flex items-center p-4 border-b"
+        //             >
+        //                 <div className="w-10 h-10 rounded-full bg-yellow-200 mr-3" />
+        //                 <div className="flex-1">
+        //                     <div className="font-medium">{chat.name}</div>
+        //                     <div className="text-sm text-gray-500 truncate">
+        //                         {chat.lastMessage}
+        //                     </div>
+        //                 </div>
+        //                 <div className="text-xs text-gray-400">{chat.timestamp}</div>
+        //             </div>
+        //         ))}
+        //     </div>
+        // </div>
     )
 }
 
