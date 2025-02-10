e51e304af2f2be38f61300ce9518b18eb439f814
eyeben <qkrehdals97@naver.com>
2025-02-10 14:03:43 +0900

    Feat: Redis 세팅
    Config 파일 수정, 메시지관련 경로 설정

--- Backend/IsCream/src/main/java/com/ssafy/iscream/common/config/RedisConfig.java
+++ Backend/IsCream/src/main/java/com/ssafy/iscream/common/config/RedisConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
 
 @Configuration
 public class RedisConfig {
@@ -20,4 +25,28 @@ public class RedisConfig {
         return new LettuceConnectionFactory(host, port);
     }
 
+    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory) {
+
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+
+        return container;  // 채팅방이 생성될 때 동적으로 구독 추가 예정
+    }
+
+    @Bean
+    public MessageListenerAdapter messageListener() {
+        return new MessageListenerAdapter();
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
 }
