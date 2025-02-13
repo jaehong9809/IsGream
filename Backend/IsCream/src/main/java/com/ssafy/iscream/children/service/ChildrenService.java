a97eae77d1bbeeeba6cfac15b301b3e1b483e2c0
권민채 <61741340+MinchaeKwon@users.noreply.github.com>
2025-02-13 14:44:36 +0900

    Feat: 자녀 일괄 삭제 시 htp 검사 파일 s3에서 삭제

--- Backend/IsCream/src/main/java/com/ssafy/iscream/children/service/ChildrenService.java
+++ Backend/IsCream/src/main/java/com/ssafy/iscream/children/service/ChildrenService.java
@@ -9,7 +9,11 @@ import com.ssafy.iscream.children.dto.res.ChildrenGetRes;
 import com.ssafy.iscream.children.repository.ChildRepository;
 import com.ssafy.iscream.common.exception.ErrorCode;
 import com.ssafy.iscream.common.exception.MinorException.DataException;
+import com.ssafy.iscream.htpTest.domain.HtpTest;
+import com.ssafy.iscream.htpTest.service.HtpSelectService;
+import com.ssafy.iscream.s3.service.S3Service;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -62,6 +66,7 @@ public class ChildrenService {
 
         childOriginal.updateChild(childrenUpdateReq.getNickname(), childrenUpdateReq.getBirthDate(),
                 Gender.valueOf(childrenUpdateReq.getGender()));
+
 //        childRepository.save(childOriginal);
     }
 
@@ -79,6 +84,30 @@ public class ChildrenService {
 
     public Child getById(Integer childId) {
         return childRepository.findById(childId).orElseThrow(() -> new DataException(ErrorCode.DATA_NOT_FOUND));
+    }
+
+    private final S3Service s3Service;
+    private final HtpSelectService htpSelectService;
+
+    // 자녀와 관련된 정보 일괄 삭제
+    // htp/big5 pdf, image url 삭제 및 s3에 업로드 된 파일 삭제
+    @Scheduled(cron = "0 */30 * * * ?")
+    @Transactional
+    public void deleteChildrenFile(Integer childId) {
+        List<HtpTest> htpTestList = htpSelectService.getHtpTestList(childId);
+
+        for (HtpTest htpTest : htpTestList) {
+            s3Service.deleteFile(htpTest.getPdfUrl());
+            s3Service.deleteFile(htpTest.getHouseDrawingUrl());
+            s3Service.deleteFile(htpTest.getTreeDrawingUrl());
+            s3Service.deleteFile(htpTest.getMaleDrawingUrl());
+            s3Service.deleteFile(htpTest.getFemaleDrawingUrl());
+        }
+
+        // big five 검사 내역 조회 -> pdf 삭제
+
+
+        childRepository.deleteById(childId);
     }
 
 }
