c5293aaac18a55c5677ae3cdea510069bdb17dec
권민채 <61741340+MinchaeKwon@users.noreply.github.com>
2025-02-14 10:41:23 +0900

    Fix: 좋아요 정렬 로직 수정 완료
    - 정렬 사용 시 post에 likeCount 저장
    - 좋아요순 정렬 시 db에 좋아요수 동기화

--- Backend/IsCream/src/main/java/com/ssafy/iscream/board/repository/PostRepository.java
+++ Backend/IsCream/src/main/java/com/ssafy/iscream/board/repository/PostRepository.java
@@ -5,10 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Set;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
     List<Post> findTop5ByOrderByCreatedAtDescPostIdDesc();
-    List<Post> findByPostIdIn(List<Integer> postIds);
 }
