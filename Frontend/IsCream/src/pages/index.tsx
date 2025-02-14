513124b5971cd1b7e20d0c0b3f5ce72ce8a8aecb
tomato <98751644+yujeong789@users.noreply.github.com>
2025-02-14 17:41:17 +0900

    Chore: Git 로그 처리

--- Frontend/IsCream/src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-export { default as CalendarPage } from "./CalendarPage";
-// export { default as MyReport } from "./mypage/MyReport";
-export { default as ChangeInfo } from "./mypage/ChangeInfo";
-export { default as LoginPage } from "./LoginPage";
-export { default as MyPage } from "./MyPage";
-export { default as MainPage } from "./MainPage";
-export { default as BoardMain } from "./baord/BoardMain";
