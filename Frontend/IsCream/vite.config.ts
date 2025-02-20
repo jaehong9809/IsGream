a976dafbcb750b4d0018b232d7dc36a7d7885638
boseong <qkrqhd23@naver.com>
2025-02-20 12:33:29 +0900

    Feat: PWA 기능

--- Frontend/IsCream/vite.config.ts
+++ Frontend/IsCream/vite.config.ts
@@ -11,27 +11,66 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       manifest: {
-        name: "My PWA App",
-        short_name: "PWA App",
-        description: "A simple PWA using React and Vite",
+        name: "아이's그림",
+        short_name: "아이's그림",
+        description: "AI가 해주는 아이심리 검사사",
         theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "fullscreen",
+        orientation: "portrait",
+        scope: ".",
+        start_url: ".",
+        categories: ["education", "kids"],
         icons: [
           {
-            src: "/pwa-icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
+            src: "/logo.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any"
           },
           {
-            src: "/pwa-icon-512x512.png",
+            src: "/logo.png",
             sizes: "512x512",
-            type: "image/png"
+            type: "image/png",
+            purpose: "maskable"
+          },
+          {
+            src: "/logo.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any"
           }
         ]
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 4 * 1024 * 1024, // 4MB 제한
+        clientsClaim: true,
+        cleanupOutdatedCaches: true,
+        disableDevLogs: true,
+        navigateFallback: "index.html",
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/i12a407\.p\.ssafy\.io/,
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "api-cache",
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
+        ]
+      },
+      devOptions: {
+        enabled: false, // 개발 모드에서 서비스 워커 비활성화
+        type: "module"
       }
     })
   ],
   define: {
-    "process.env": process.env
+    "process.env": process.env,
+    VITE_KAKAO_MAP_API_KEY: JSON.stringify(process.env.VITE_KAKAO_MAP_API_KEY)
   },
   server: {
     proxy: {
